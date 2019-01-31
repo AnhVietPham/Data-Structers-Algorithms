545b73e40a6a95b359d80bbcde040da0b0837362
Anh Viet Pham <pham.anh.viet.vlth@gmail.com>
2019-01-31 14:45:30 +0700

    Add performance calculate time finding Nemo

--- Big0/findNemo.js
+++ Big0/findNemo.js
@@ -1,13 +1,33 @@
 // For loop in Javascript
-
+const {
+    performance,
+    PerformanceObserver
+  } = require('perf_hooks');
+  
 const nemo = ['nemo'];
+const fish = ['dory', 'bruce','marlin','nemo'];
+const everyone = ['dory','bruce','marlin','nemo','gill','bloat','nigel','squirt','darla','hank'];
+const large = new Array(1000000).fill('nemo');
+// function findNemo(array){
+//     for (let i = 0; i < array.length; i++){
+//         if(array[i] === 'nemo'){
+//             console.log('Found Nemo');
+//         }
+//     }
+// }
 
-function findNemo(array){
-    for (let i = 0; i < array.length; i++){
+// findNemo(nemo);
+
+function findNemo2(array){
+    let t0 = performance.now();
+    for(let i = 0; i < array.length; i++){
+        console.log(i)
         if(array[i] === 'nemo'){
-            console.log('Found Nemo');
+            console.log('Found Nemo!');
         }
     }
+    let t1 = performance.now();
+    console.log("Call to find Nemo took " + (t1 - t0) + " miliseconds");
 }
 
-findNemo(nemo);
+findNemo2(fish);
