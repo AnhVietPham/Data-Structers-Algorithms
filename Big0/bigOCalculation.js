c08007447c134cebdf16e0c360f6fe74611279e8
Anh Viet Pham <pham.anh.viet.vlth@gmail.com>
2019-02-13 22:12:56 +0700

    Big O calculation example 3

--- Big0/bigOCalculation.js
+++ Big0/bigOCalculation.js
@@ -37,4 +37,24 @@ function anotherFunChallenge(input) {
 }
 
 //BIG O(4 + 7n)
-// Remove constant --> BIG O[n]
+// Remove constant --> BIG O(n)
+
+// Example 3
+function printFirstItemThenFirstHalfThenSayHi100Times(items) {
+    console.log(items[0]); // O(1)
+
+    var middleIndex = Math.floor(items.length / 2);
+    var index = 0;
+
+    while (index < middleIndex) { // O(n/2)
+        console.log(items[index]);
+        index++;
+    }
+
+    for (var i = 0; i < 100; i++) { // O(100)
+        console.log("Hi");
+    }
+}
+
+// O(1 + n/2 + 100) --> O(n/2 + 101)
+// Remove constant time --> O(n)
