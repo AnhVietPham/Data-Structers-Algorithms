7badf6c08270176d3ae6242cdc327090e63f5443
Anh Viet Pham <pham.anh.viet.vlth@gmail.com>
2019-02-13 22:21:24 +0700

    Big O calculation Example 4

--- Big0/bigOCalculation.js
+++ Big0/bigOCalculation.js
@@ -1,39 +1,39 @@
-
+// Rule Worst Case
 // Example 1
 function funChallenge(input) {
-    let a = 10; // O[1]
-    a = 50 + a; // O[1]
+    let a = 10; // O(1)
+    a = 50 + a; // O(1)
 
-    for (let i = 0; i < input.length; i++) {  // O[n]
-        anotherFunction(); // O[n]
-        let stranger = true; // O[n]
-        a++; // O[n]
+    for (let i = 0; i < input.length; i++) {  // O(n)
+        anotherFunction(); // O(n)
+        let stranger = true; // O(n)
+        a++; // O(n)
     }
-    return a // O[1]
+    return a // O(1)
 }
 
 // Big O(3 + 4n) 
-// Remove constants --> Big O(n)
+// Rule Remove constants --> Big O(n)
 
 
 // Example 2
 function anotherFunChallenge(input) {
-    let a = 5; // O[1]
-    let b = 10; // O[1]
-    let c = 50; // O[1]
+    let a = 5; // O(1)
+    let b = 10; // O(1)
+    let c = 50; // O(1)
 
-    for (let i = 0; i < input; i++) { // O[n]
-        let x = i + 1; // O[n]
-        let y = i + 2; // O[n]
-        let z = i + 3; // O[n]
+    for (let i = 0; i < input; i++) { // O(n)
+        let x = i + 1; // O(n)
+        let y = i + 2; // O(n)
+        let z = i + 3; // O(n)
     }
 
-    for (let j = 0; i < input; j++) { // O[n]
-        let p = j * 2; // O[n]
-        let q = j * 2; // O[n]
+    for (let j = 0; i < input; j++) { // O(n)
+        let p = j * 2; // O(n)
+        let q = j * 2; // O(n)
     }
 
-    let whoAmI = "I don't know"; // O[1]
+    let whoAmI = "I don't know"; // O(1)
 }
 
 //BIG O(4 + 7n)
@@ -58,3 +58,17 @@ function printFirstItemThenFirstHalfThenSayHi100Times(items) {
 
 // O(1 + n/2 + 100) --> O(n/2 + 101)
 // Remove constant time --> O(n)
+
+//Example 4
+function compressBoxesTwice(boxes, boxes2) {
+    boxes.forEach(element => { // O(a)
+        console.log(boxes)
+    });
+
+    boxes2.forEach(element => { // O(b)
+        console.log(boxes)
+    });
+}
+
+// Rule Different term for input
+// Big O(a + b)
