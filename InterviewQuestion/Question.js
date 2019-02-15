e46e4008b8010a425d3e67e72199506e8baf6a6d
Anh Viet Pham <pham.anh.viet.vlth@gmail.com>
2019-02-15 09:51:19 +0700

    Interview Question Solution 2 Using Object in JavaScript

--- InterviewQuestion/Question.js
+++ InterviewQuestion/Question.js
@@ -10,8 +10,9 @@
     const array2 = ['z', 'y', 'i'];
     should return true
 */
-const array1 = ['a','b','c','x'];
-const array2 = ['z', 'y', 'a'];
+// Solution 1
+const array1 = ['a', 'b', 'c', 'x'];
+const array2 = ['z', 'y', 'c'];
 function containsCommonItem(arr1, arr2) {
     for (let i = 0; i < arr1.length; i++) {
         for (let j = 0; j < arr2.length; j++) {
@@ -23,6 +24,35 @@ function containsCommonItem(arr1, arr2) {
     return false
 }
 
-console.log(containsCommonItem(array1,array2));
-
+//console.log(containsCommonItem(array1,array2));
 // O(a * b)
+
+// Create object in Javascript
+const person = {
+    name: 'Anh Viet Pham',
+    age: 23,
+    gender: 'male',
+    interests: ['Music', 'Soccer'],
+    address: 'HCM City'
+};
+//console.log(person['age']);
+
+// Soluton 2 --> Usng Object in JavaScript
+function containsCommonItem2(arr1, arr2) {
+    let map = {};
+    for (let i = 0; i < arr1.length; i++) {
+        if (!map[arr1[i]]) {
+            const item = arr1[i]
+            map[item] = true;
+        }
+    }
+
+    for (let j = 0; j < arr2.length; j++) {
+        if (map[arr2[j]]) {
+            return true;
+        }
+    }
+    return false;
+}
+console.log(containsCommonItem2(array1, array2));
+// O(a + b)
