6ab901f09cb787c6c8d757af78184711484ee106
Anh Viet Pham <pham.anh.viet.vlth@gmail.com>
2019-02-18 06:44:26 +0700

    Reference Type

---
+++ Data-Structure/DemoClass.js
@@ -0,0 +1,19 @@
+// Reference type
+var obj1 = { value: 10 };
+var obj2 = obj1;
+var obj3 = {value : '10'};
+console.log(obj1, obj2);
+console.log(obj1 === obj2);
+console.log(obj1, obj3);
+console.log(obj1.value === obj3.value);
+let name = 'AVP';
+let name1 = name;
+console.log(name,name1);
+name = "Another AVP";
+console.log(name,name1)
+
+// Context vs scope
+
+
+
+// instantiation
