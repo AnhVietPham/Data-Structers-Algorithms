e9681a11e1ef4dcf3fd717abce1245eb16343984
Anh Viet Pham <pham.anh.viet.vlth@gmail.com>
2019-07-21 15:21:01 +0700

    init class Stack

---
+++ Data-Structure/Stack+Queues/Stack+Queues.js
@@ -0,0 +1,26 @@
+class Node {
+    constructor(value) {
+        this.next = null;
+        this.value = value;
+    }
+}
+
+class Stack {
+    constructor(value) {
+        this.top = null;
+        this.bottom = null;
+        this.length++;
+    }
+
+    peek() {
+
+    }
+
+    pop() {
+
+    }
+
+    push() {
+
+    }
+}
