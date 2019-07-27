20e4a5043053c07dc7bbcbc53c2d6fbb53d5f2f9
Anh Viet Pham <pham.anh.viet.vlth@gmail.com>
2019-07-27 21:23:15 +0700

    init class Queue using Stack

---
+++ InterviewQuestion/Implement-Queue-using-Stacks.js
@@ -0,0 +1,45 @@
+class Node {
+    constructor(value) {
+        this.node = null;
+        this.value = value;
+    }
+}
+
+class Stack {
+    constructor() {
+        this.top = null;
+        this.bottom = null;
+        this.length = 0;
+    }
+
+    peek() {
+        return this.top;
+    }
+
+    pop() {
+        if (!this.top) {
+            return null;
+        }
+        this.top = this.top.next;
+        this.length--;
+        return this;
+    }
+
+    push(value) {
+        const newNode = new Node(value);
+        if (this.length === 0) {
+            this.top = newNode;
+            this.bottom = newNode;
+        } else {
+            const holdingPointer = this.top;
+            this.top = newNode;
+            this.top.next = holdingPointer;
+        }
+        this.length++;
+        return this;
+    }
+}
+
+class SpecicalQueue{
+
+}
