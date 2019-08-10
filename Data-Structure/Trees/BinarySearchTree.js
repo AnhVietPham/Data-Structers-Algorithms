a4d5652ad58f6edfb8bd252ffb35e85184b398db
Anh Viet Pham <pham.anh.viet.vlth@gmail.com>
2019-08-10 22:39:53 +0700

    Homework: Init BinarySearchTree

---
+++ Data-Structure/Trees/BinarySearchTree.js
@@ -0,0 +1,21 @@
+class Node {
+    constructor(value) {
+        this.left = null;
+        this.right = null;
+        this.value = value;
+    }
+}
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+
+    insert(value) {
+
+    }
+
+    lookup(value) {
+
+    }
+}
