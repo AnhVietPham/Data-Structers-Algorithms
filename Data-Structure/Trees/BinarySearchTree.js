0acbb61b52f26b05568cf1e1c85dc78147de06ac
Anh Viet Pham <pham.anh.viet.vlth@gmail.com>
2019-08-10 23:48:43 +0700

    Homework: Tree add method insert()

--- Data-Structure/Trees/BinarySearchTree.js
+++ Data-Structure/Trees/BinarySearchTree.js
@@ -12,10 +12,47 @@ class BinarySearchTree {
     }
 
     insert(value) {
-
+        const newNode = new Node(value);
+        if (!this.root) {
+            this.root = newNode;
+            return this;
+        } else {
+            let currentNode = this.root;
+            while (currentNode) {
+                if (value < currentNode.value) {
+                    if (!currentNode.left) {
+                        currentNode.left = newNode;
+                        return this;
+                    }
+                    currentNode = currentNode.left;
+                } else {
+                    if (!currentNode.right) {
+                        currentNode.right = newNode;
+                        return this;
+                    }
+                    currentNode = currentNode.right;
+                }
+            }
+        }
     }
 
     lookup(value) {
 
     }
+}
+
+const tree = new BinarySearchTree();
+tree.insert(9)
+tree.insert(3)
+tree.insert(5)
+tree.insert(21)
+tree.insert(130)
+tree.insert(154)
+tree.insert(1)
+console.log(JSON.stringify(traverse(tree.root)));
+function traverse(node) {
+    const tree = { value: node.value };
+    tree.left = node.left === null ? null : traverse(node.left);
+    tree.right = node.right === null ? null : traverse(node.right);
+    return tree;
 }
