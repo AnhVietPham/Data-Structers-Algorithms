3f0498f7e30632899753f6b08f93b2b9943bd884
Anh Viet Pham <pham.anh.viet.vlth@gmail.com>
2019-08-16 17:18:24 +0700

    HomeWork : BST Done remove entry in tree

--- Data-Structure/Trees/BinarySearchTree.js
+++ Data-Structure/Trees/BinarySearchTree.js
@@ -77,23 +77,68 @@ class BinarySearchTree {
                         return this;
                     }
                 } else if (!currentNode.left) {
-                    if (currentNode.value < parentNode.value){
+                    if (currentNode.value < parentNode.value) {
                         parentNode.left = currentNode.right;
                         return this;
-                    }else {
+                    } else {
                         parentNode.right = currentNode.right;
                         return this;
                     }
-                } else if (!currentNode.right){
-                    if (currentNode.value < parentNode.value){
+                } else if (!currentNode.right) {
+                    if (currentNode.value < parentNode.value) {
                         parentNode.left = currentNode.left;
                         return this;
-                    }else {
+                    } else {
                         parentNode.right = currentNode.left;
                         return this;
                     }
-                } else if (currentNode.left && currentNode.right){
-                    
+                } else if (currentNode.left && currentNode.right) {
+                    let leftMostParent = currentNode.left;
+                    if ((!leftMostParent.right) || (!leftMostParent.right && !leftMostParent.left)) {
+                        if (currentNode.value < parentNode.value) {
+                            parentNode.left = leftMostParent;
+                            return this;
+                        } else {
+                            parentNode.right = leftMostParent;
+                            return this;
+                        }
+                    } else if (leftMostParent.right) {
+                        let leftMostChild = leftMostParent.right;
+                        if (!leftMostChild.right || (!leftMostChild.right && !leftMostChild.left)) {
+                            if (currentNode.value < parentNode.value) {
+                                parentNode.left = leftMostChild;
+                                leftMostParent.right = leftMostChild.left;
+                                parentNode.left.left = leftMostParent;
+                                parentNode.left.right = currentNode.right;
+                                return this
+                            } else {
+                                parentNode.right = leftMostChild;
+                                leftMostParent.right = leftMostChild.left;
+                                parentNode.right.left = leftMostParent;
+                                parentNode.right.right = currentNode.right;
+                                return this;
+                            }
+                        } else if (leftMostChild.right) {
+                            let currentRightNodeOfLeftMostChild = leftMostChild.right;
+                            let subCurrentOfRightNodeOfLeftMostChild = currentRightNodeOfLeftMostChild.right
+                            while (subCurrentOfRightNodeOfLeftMostChild) {
+                                leftMostChild = currentRightNodeOfLeftMostChild;
+                                currentRightNodeOfLeftMostChild = subCurrentOfRightNodeOfLeftMostChild;
+                                subCurrentOfRightNodeOfLeftMostChild = subCurrentOfRightNodeOfLeftMostChild.right;
+                            }
+                            leftMostChild.right = subCurrentOfRightNodeOfLeftMostChild;
+
+                            if (currentNode.value < parentNode.value) {
+                                parentNode.left = currentRightNodeOfLeftMostChild;
+                                currentRightNodeOfLeftMostChild.left = leftMostParent;
+                                return this;
+                            } else {
+                                parentNode.right = currentRightNodeOfLeftMostChild;
+                                currentRightNodeOfLeftMostChild.left = leftMostParent;
+                                return this;
+                            }
+                        }
+                    }
                 }
             }
         }
@@ -101,17 +146,26 @@ class BinarySearchTree {
 }
 
 const tree = new BinarySearchTree();
-tree.insert(9)
-tree.insert(3)
-tree.insert(5)
+tree.insert(44)
+tree.insert(17)
+tree.insert(88)
+tree.insert(8)
+tree.insert(32)
+tree.insert(28)
 tree.insert(21)
-tree.insert(130)
-tree.insert(154)
-tree.insert(1)
-tree.insert(2)
+tree.insert(29)
+tree.insert(65)
+tree.insert(97)
+tree.insert(54)
+tree.insert(82)
+tree.insert(93)
+tree.insert(76)
+tree.insert(68)
+tree.insert(80)
 tree.insert(6)
-tree.remove(5)
-tree.remove(1)
+tree.insert(85)
+tree.insert(87)
+tree.remove(88)
 console.log(JSON.stringify(traverse(tree.root)));
 function traverse(node) {
     const tree = { value: node.value };
