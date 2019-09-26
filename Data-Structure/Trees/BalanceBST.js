199d45e8ddedb56b1871dfc6ae4a0a904e129dc4
Anh Viet Pham <pham.anh.viet.vlth@gmail.com>
2019-09-26 22:47:09 +0700

    Add BalanceBST

---
+++ Data-Structure/Trees/BalanceBST.js
@@ -0,0 +1,40 @@
+class Node {
+    constructor(value) {
+        this.left = null;
+        this.right = null;
+        this.value = value;
+    }
+}
+
+class BST {
+    constructor() {
+        this.root = null;
+    }
+
+    insert(value) {
+        const newNode = new Node(value);
+        if (!this.root) {
+            this.root = newNode;
+        } else {
+            var currentNode = this.root;
+            while (currentNode) {
+                if (value < currentNode.value) {
+                    if (!currentNode.left) {
+                        currentNode.left = newNode;
+                        return this;
+                    }
+                    currentNode = currentNode.left
+                } else {
+                    if (value > currentNode.value) {
+                        if (!currentNode.right) {
+                            currentNode.right = newNode;
+                            return this;
+                        }
+                        currentNode = currentNode.right;
+                    }
+                }
+            }
+        }
+    }
+
+}
