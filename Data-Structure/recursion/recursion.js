946aee9da23516ca3c46aebee4320504d775969c
Anh Viet Pham <pham.anh.viet.vlth@gmail.com>
2019-10-10 21:56:23 +0700

    Fibonancy Recursion

--- Data-Structure/recursion/recursion.js
+++ Data-Structure/recursion/recursion.js
@@ -5,7 +5,7 @@ var sumRecursion = function (value) {
     return sumRecursion(value - 1) + value;
 }
 
-var sumRecursionReduction = function (value) {
+var sumRecursionReduction = function(value) {
     var sum = 0;
     for (var i = 0; i <= value; i++) {
         sum += i
@@ -20,6 +20,45 @@ var sumRecursionTail = function (value, x = 0) {
     return sumRecursionTail(value - 1, value + x);
 }
 
-console.log(sumRecursion(10))
-console.log(sumRecursionReduction(10))
-console.log(sumRecursionTail(10))
+// 1 1 2 3 5 8 13 20 32
+
+var fibonacyReduction = function (value) {
+    var n1 = 0;
+    var n2 = 1;
+    var temp = 1;
+    if (value < 0) {
+        return -1;
+    } else if (value == 0 || value == 1) {
+        return 1;
+    } else {
+        for (var i = 2; i < value; i++) {
+            n1 = n2;
+            n2 = temp;
+            temp = n1 + n2;
+        }
+    }
+    return temp;
+}
+
+var fibonacyRecursion = function (value) {
+    if (value == 0 || value == 1) {
+        return 1;
+    }
+    return fibonacyRecursion(value - 1) + fibonacyRecursion(value - 2);
+}
+
+// Calculate sum n 
+// console.log(sumRecursion(10))
+// console.log(sumRecursionReduction(10))
+// console.log(sumRecursionTail(10))
+
+
+// // Calculate fibonacy recursion reduction
+// for (var i = 0; i < 5; i++) {
+//     console.log(fibonacyReduction(i));
+// }
+
+// Calculate fibonancy recursion
+for (var i = 0; i < 5; i++) {
+    console.log(fibonacyRecursion(i));
+}
