b1f127a1069d021659b995a8e8e4fd057e94532c
Anh Viet Pham <pham.anh.viet.vlth@gmail.com>
2019-10-14 14:40:59 +0700

    NonLinearRecursion

--- Data-Structure/recursion/recursion.js
+++ Data-Structure/recursion/recursion.js
@@ -1,6 +1,6 @@
 /**
  * Linear Recursion 
- */ 
+ */
 var sumLinearRecursion = function (value) {
     if (value == 0) {
         return 0;
@@ -65,6 +65,56 @@ var fibonancyRecurionTail = function (value, n1, n2) {
     return fibonancyRecurionTail(value - 1, n2, n1 + n2);
 }
 
+/**
+ * Mutual Recursion
+ */
+var mutualRecursionXn = function (value) {
+    if (value == 0) {
+        return 1;
+    }
+    return mutualRecursionXn(value - 1) + mutualRecursionYn(value - 1);
+}
+
+var mutualRecursionYn = function (value) {
+    if (value == 0) {
+        return 0;
+    }
+    return 3 * mutualRecursionXn(value - 1) + 2 * mutualRecursionYn(value - 1);
+}
+
+/**
+ * From MutualRecursion to Binary Recursion
+ */
+var binaryRecursionXn = function (value) {
+    if (value <= 0) {
+        return 1;
+    }
+    return 3 * binaryRecursionXn(value - 1) + binaryRecursionXn(value - 2);
+}
+
+var binaryRecursionYn = function (value) {
+    if (value <= 0) {
+        return 0;
+    }
+    return binaryRecursionYn(value - 2) + 3 * binaryRecursionYn(value - 1);
+}
+
+/**
+ * NonLinear Recursion
+ */
+var nonLinearRecursion = function (value) {
+    if (value == 0) {
+        return 1;
+    }
+
+    var s = 0;
+
+    for (var i = 0; i <= value; i++) {
+        s = s + i * i * nonLinearRecursion(value - 1);
+    }
+    return s;
+}
+
 // Calculate sum n 
 // console.log(sumRecursion(10))
 // console.log(sumRecursionReduction(10))
@@ -81,5 +131,13 @@ var fibonancyRecurionTail = function (value, n1, n2) {
 //     console.log(fibonacyRecursion(i));
 // }
 // console.log(fibonancyRecurionTail(4, 0, 1));
-console.log(sumLinearRecurion(5))
-console.log(sumLinerRecursionTail(5))
+// console.log(sumLinearRecurion(5))
+// console.log(sumLinerRecursionTail(5))
+
+// console.log(mutualRecursionYn(3));
+// console.log(mutualRecursionXn(3));
+
+// console.log(binaryRecursionXn(3));
+// console.log(binaryRecursionYn(3));
+
+console.log(nonLinearRecursion(4))
